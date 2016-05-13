5da3726ec46c24335f7044dd160a2e6d830ffea9
Tobias Kuhn <kuhntobias@gmail.com>
2016-05-13 17:05:37 +0200

    Hello world with Thymeleaf

--- src/main/java/org/petapico/hubber/Hubber.java
+++ src/main/java/org/petapico/hubber/Hubber.java
@@ -1,9 +1,16 @@
 package org.petapico.hubber;
 
 import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Hubber {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
+	public static void main(String[] args) {
+		Map<String,String> map = new HashMap<>();
+		map.put("name", "Sam");
+		get("/hello", (rq, rs) -> new ModelAndView(map, "hello"), new ThymeleafTemplateEngine());
+	}
 }
