3121c555cc897503df58307d6a87cca771173ee7
Tobias Kuhn <kuhntobias@gmail.com>
2016-05-14 10:08:42 +0200

    Use HTML5

--- src/main/java/org/petapico/hubber/Hubber.java
+++ src/main/java/org/petapico/hubber/Hubber.java
@@ -7,10 +7,19 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.thymeleaf.resourceresolver.ClassLoaderResourceResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
 public class Hubber {
 	public static void main(String[] args) {
 		Map<String,String> map = new HashMap<>();
 		map.put("name", "Sam");
-		get("/hello", (rq, rs) -> new ModelAndView(map, "hello"), new ThymeleafTemplateEngine());
+		TemplateResolver tr = new TemplateResolver();
+		tr.setTemplateMode("HTML5");
+		tr.setPrefix("templates/");
+		tr.setSuffix(".html");
+		tr.setCacheTTLMs(3600000L);
+		tr.setResourceResolver(new ClassLoaderResourceResolver());
+		get("/hello", (rq, rs) -> new ModelAndView(map, "hello"), new ThymeleafTemplateEngine(tr));
 	}
 }
