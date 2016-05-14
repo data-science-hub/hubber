25d01dbf45043ea585838085378e5707f0cae13e
Tobias Kuhn <kuhntobias@gmail.com>
2016-05-14 15:52:51 +0200

    Make index page

--- src/main/java/org/petapico/hubber/Hubber.java
+++ src/main/java/org/petapico/hubber/Hubber.java
@@ -11,15 +11,24 @@ import org.thymeleaf.resourceresolver.ClassLoaderResourceResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
 public class Hubber {
-	public static void main(String[] args) {
-		Map<String,String> map = new HashMap<>();
-		map.put("name", "Sam");
+
+	static ThymeleafTemplateEngine tempEngine;
+
+	static {
 		TemplateResolver tr = new TemplateResolver();
 		tr.setTemplateMode("HTML5");
 		tr.setPrefix("templates/");
 		tr.setSuffix(".html");
 		tr.setCacheTTLMs(3600000L);
 		tr.setResourceResolver(new ClassLoaderResourceResolver());
-		get("/hello", (rq, rs) -> new ModelAndView(map, "hello"), new ThymeleafTemplateEngine(tr));
+		tempEngine = new ThymeleafTemplateEngine(tr);
 	}
+
+	public static void main(String[] args) {
+		Map<String,String> map = new HashMap<>();
+		map.put("title", "Hubber");
+		map.put("message", "This is Hubber.");
+		get("/", (rq, rs) -> new ModelAndView(map, "index"), tempEngine);
+	}
+
 }
