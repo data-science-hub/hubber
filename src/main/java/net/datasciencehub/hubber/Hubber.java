3a59ccf823e10f4b65c6319bb9daa6ec7a89315b
Tobias Kuhn <kuhntobias@gmail.com>
2016-05-20 16:34:26 +0200

    Preliminary code to request access token

--- src/main/java/net/datasciencehub/hubber/Hubber.java
+++ src/main/java/net/datasciencehub/hubber/Hubber.java
@@ -2,14 +2,24 @@ package net.datasciencehub.hubber;
 
 import static spark.Spark.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
 import org.thymeleaf.resourceresolver.ClassLoaderResourceResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
 import spark.ModelAndView;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import spark.utils.IOUtils;
 
 public class Hubber {
 
@@ -18,7 +28,6 @@ public class Hubber {
 	private static String orcidClientId = "APP-W02BIN0XPD5T5PFL";
 	private static String orcidScope = "/authenticate";
 	private static String orcidRedirectUrl = "http://hubber.tkuhn.eculture.labs.vu.nl/login";
-	//private static String orcidRedirectUrl = "https://developers.google.com/oauthplayground";
 
 	static {
 		TemplateResolver tr = new TemplateResolver();
@@ -42,7 +51,22 @@ public class Hubber {
 				"redirect_uri=" + orcidRedirectUrl);
 		get("/", (rq, rs) -> new ModelAndView(map, "index"), tempEngine);
 		get("/login", (rq, rs) -> {
-			System.err.println("Code: " + rq.queryParams("code"));
+			String authCode = rq.queryParams("code");
+			System.err.println("Authentication code: " + authCode);
+			HttpPost post = new HttpPost("https://orcid.org/oauth/token");
+			post.setHeader("Accept", "application/json");
+			List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+			urlParameters.add(new BasicNameValuePair("client_id", orcidClientId));
+			urlParameters.add(new BasicNameValuePair("client_secret", ""));
+			urlParameters.add(new BasicNameValuePair("grant_type", "authorization_code"));
+			urlParameters.add(new BasicNameValuePair("redirect_uri", orcidRedirectUrl));
+			urlParameters.add(new BasicNameValuePair("code", authCode));
+			post.setEntity(new UrlEncodedFormEntity(urlParameters));
+			HttpClient client = HttpClientBuilder.create().build();
+			HttpResponse response = client.execute(post);
+			System.err.println("Status Code: " + response.getStatusLine().getStatusCode());
+			String respString = IOUtils.toString(response.getEntity().getContent());
+			System.err.println("Response: " + respString);
 			return "";
 		});
 	}
